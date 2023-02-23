f0ae2f0f81872be0078c5bb0abede195f18b6ac3
RAHUL RAMAN <58595285+rahul7070@users.noreply.github.com>
2023-02-23 22:42:42 +0530

    dsfsd

--- admin.js
+++ admin.js
@@ -1,4 +1,61 @@
-let url = `http://localhost:3000/recipes`;
+let url = `http://localhost:3000`;
+
+let btn = document.querySelector(".addBtn");
+
+let newprod = {
+    name : "Roti Sabzi",
+    price: "250",
+    category: "non-veg"
+}
 
 
-fetch(url).then((res)=>res.json()).then((data)=>console.log(data));
+btn.addEventListener("click", ()=>{
+    addNewProduct(newprod);
+    addUser(newuser)
+})
+
+async function addNewProduct(newprod){
+    try {
+        let req = await fetch(`${url}/recipes`, {
+            method : "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(newprod)
+        })
+        console.log(req);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function addUser(newuser){
+    try {
+        let req = await fetch(`${url}/users`, {
+            method : "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(newuser)
+        })
+        console.log(req);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+let foodsaleobj = {
+    burger : 50,
+    beverage : 20,
+    dessert : 10,
+    "biryani bucket": 27,
+    "box meals" : 71,
+    "new launch" : 15,
+    "chicken bucket" : 44
+}
+localStorage.setItem("compData", JSON.stringify(foodsaleobj));
+
+
+
+
